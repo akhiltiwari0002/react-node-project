cfd460c0dcee9b6fa1ffb4b3b2163b4acdb71aec
Akhil <akhiltiwari0002@gmail.com>
2024-01-06 14:08:25 +0530

    project movement

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'), // Use 'static' instead of 'contentBase'
+    port: 3000,
+  },
+};
